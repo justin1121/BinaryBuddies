d11898ede1e1ecd0b0b536b764ddfce895fcc4e3
Justin Patriquin <justinpatriquin@gmail.com>
2013-07-22 17:30:50 +0000

    Code template

---
+++ bbuddies.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void * get_memory(int);
+void * grow_memory(int, void *);
+void * pregrow_memory(int, void *);
+void release_memory(void *);
+int start_memory(int);
+void end_memory(void);
+
+int main(int argc, int * argv[]){
+
+}
+
+void * get_memory(int size){
+
+}
+
+void * grow_memory(int size, void * mem){
+
+} 
+
+void * pregrow_memory(int size, void * mem){
+
+}
+
+void release_memory(void * mem){
+
+}
+
+int start_memory(int size){
+
+}
+
+void end_memory(void){
+
+}
