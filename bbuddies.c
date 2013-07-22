8ec68b9700ab06802bf2bbf8e3af09767d33368e
Justin Patriquin <justinpatriquin@gmail.com>
2013-07-22 18:28:12 +0000

    bbuddies compiler warning removed

--- bbuddies.c
+++ bbuddies.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void * get_memory(int);
-void * grow_memory(int, void *);
-void * pregrow_memory(int, void *);
-void release_memory(void *);
-int start_memory(int);
-void end_memory(void);
-
-int main(int argc, int * argv[]){
-
-}
-
 void * get_memory(int size){
-
+  return NULL;
 }
 
 void * grow_memory(int size, void * mem){
+  return NULL;
 
 } 
 
 void * pregrow_memory(int size, void * mem){
+  return NULL;
 
 }
 
@@ -29,7 +20,7 @@ void release_memory(void * mem){
 }
 
 int start_memory(int size){
-
+  return 0;
 }
 
 void end_memory(void){
