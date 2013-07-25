d73e9aab885567ba3b0271b2873a024bb7a25d6e
Justin Patriquin <justinpatriquin@gmail.com>
2013-07-25 15:25:13 -0300

    finishedish start_memory

--- bbuddies.c
+++ bbuddies.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "buddytree.h"
 
 #define MIN_SIZE 4
+
+static int address_compare(void * a, void * b);
 
 typedef struct _mem_header{
   int size;
@@ -9,6 +12,8 @@ typedef struct _mem_header{
 } mem_header_t;
 
 void * mem;
+void * usermemoffset;
+BuddyTree * tree;
 
 void * get_memory(int size){
   return NULL;
@@ -29,21 +34,50 @@ void release_memory(void * mem){
 }
 
 int start_memory(int size){
+  int tmpsize;
+
   if((size & (size - 1)) == 0){
     int totalnodes = size / MIN_SIZE;  
-    size += sizeof(/*BTREE NODE*/int) * totalnodes * sizeof(mem_header_t) * totalnodes;
-    if((mem = malloc(size)) == NULL){
+    tmpsize = sizeof(BuddyTree) + size + 
+              (sizeof(BuddyTreeNode) * totalnodes) + 
+              (sizeof(mem_header_t) * totalnodes);
+    if((mem = malloc(tmpsize)) == NULL){
       return 0;
     }
-    else{
-      return 1;
-    } 
   }
   else{
     return 0;
   }
+
+  // set the mem in buddy tree this needs to be done
+  if((tree = BuddyTree_create(address_compare)) == NULL){
+    return 0;
+  }
+ 
+  mem_header_t * data = ((mem_header_t *)((BuddyTree *)mem) + 1);
+  data->size = size;
+  data->free = 1;
+
+  BuddyTreeNode * address = (BuddyTreeNode *)(data + 1);
+
+  // set the mem in buddy tree this needs to be done
+  if(BuddyTree_set(tree, tree, data) == -1){
+    return 0; 
+  }
+  usermemoffset = mem + (tmpsize - size);
+
+  return 1;
 }
 
 void end_memory(void){
 
+}
+
+static int address_compare(void * a, void * b){
+  if(a == b){
+    return 0;
+  } 
+  else{
+    return 1;
+  }
 }
